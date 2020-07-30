70532720f3c18ca916b8faf7b77cf2dab23f3d34
Harold <harolk1453@hotmail.com>
2020-07-30 13:31:33 -0500

    conexion

---
+++ ProyectoPlataformaW/ProyectoPlataformaW/Datos/clConexion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace ProyectoPlataformaW.Datos
+{
+    public class clConexion
+    {
+        SqlConnection conexBD = null;
+        public SqlConnection mtdConexion()
+        {
+            string cadena = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\dbProyectoPlataforma.mdf;Integrated Security=True";
+            try
+            {
+                conexBD = new SqlConnection(cadena);
+            }
+            catch (Exception error)
+            {
+
+                string err = error.Message;
+                HttpContext.Current.Response.Redirect("paginaError.aspx", true);
+                conexBD = null;
+
+
+            }
+
+            return conexBD;
+        }
+    }
+}
--- ProyectoPlataformaW/ProyectoPlataformaW/MaterPage.Master.cs
+++ ProyectoPlataformaW/ProyectoPlataformaW/MaterPage.Master.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,6 +14,11 @@ namespace ProyectoPlataformaW
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+     
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
