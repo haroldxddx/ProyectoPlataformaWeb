d36c5e1463b3ba096f8557970fc6f5fa98be2c34
Harold <harolk1453@hotmail.com>
2020-08-07 16:22:27 -0500

    Cambio de diseño formularios de registro

---
+++ ProyectoPlataformaW/ProyectoPlataformaW/Vista/registrarCurso2.aspx.cs
@@ -0,0 +1,35 @@
+﻿using ProyectoPlataformaW.Datos;
+using ProyectoPlataformaW.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProyectoPlataformaW.Vista
+{
+    public partial class registrarCurso2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegistrarP_Click(object sender, EventArgs e)
+        {
+            clEntidadCursosE objECurso = new clEntidadCursosE();
+            objECurso.Curso = txtCur.Text;
+            objECurso.Grado = dplGrado.SelectedValue;
+
+            clCursoD objCurso = new clCursoD();
+            int result = objCurso.mtdRegistrarCurso(objECurso);
+
+            if (result>0)
+            {
+                Response.Write("<script> alert(" + "'Registro Realizado Correctamente'" + ") </script>");
+                txtCur.Text = "";
+            }
+        }
+    }
+}
---
+++ ProyectoPlataformaW/ProyectoPlataformaW/Vista/registrarCurso2.aspx.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ProyectoPlataformaW.Vista
+{
+
+
+    public partial class registrarCurso2
+    {
+
+        /// <summary>
+        /// Control dplGrado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList dplGrado;
+
+        /// <summary>
+        /// Control txtCur.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCur;
+
+        /// <summary>
+        /// Control btnRegistrarC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRegistrarC;
+    }
+}
