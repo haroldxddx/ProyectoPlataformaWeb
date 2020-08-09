b4a59f3222d036def0a6541bdf8e31b691dd91fe
kimzu2000@hotmail.com <kimzu2000@hotmail.com>
2020-08-09 12:57:34 -0500

    xdd

--- ProyectoPlataformaW/ProyectoPlataformaW/Vista/registrarEstudiante.aspx.designer.cs
+++ ProyectoPlataformaW/ProyectoPlataformaW/Vista/registrarEstudiante.aspx.designer.cs
@@ -130,5 +130,14 @@ namespace ProyectoPlataformaW.Vista
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnDescargarF;
+
+        /// <summary>
+        /// Control dtgExcel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView dtgExcel;
     }
 }
