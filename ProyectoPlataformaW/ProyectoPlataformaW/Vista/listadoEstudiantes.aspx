f569b760a0a95ab9d76e73d8d2def893d7bda428
kimzu2000@hotmail.com <kimzu2000@hotmail.com>
2020-08-14 13:08:10 -0500

    xd

---
+++ ProyectoPlataformaW/ProyectoPlataformaW/Vista/listadoEstudiantes.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPageProfesor.Master" AutoEventWireup="true" CodeBehind="listadoEstudiantes.aspx.cs" Inherits="ProyectoPlataformaW.Vista.listadoEstudiantes" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    <asp:TextBox ID="txtNombre" runat="server"></asp:TextBox>
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    <asp:GridView ID="gvlistEstudiante" runat="server"></asp:GridView>
+</asp:Content>
---
+++ ProyectoPlataformaW/ProyectoPlataformaW/Vista/listadoEstudiantes.aspx.cs
@@ -0,0 +1,52 @@
+﻿using ProyectoPlataformaW.Datos;
+using ProyectoPlataformaW.Entidades;
+using ProyectoPlataformaW.Logica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProyectoPlataformaW.Vista
+{
+    public partial class listadoEstudiantes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            mtdCargarGrid();
+        }
+
+
+        private void mtdCargarGrid()
+        {
+            List<clEntidadEstudianteE> list = new List<clEntidadEstudianteE>();
+            clEstudianteD objEst = new clEstudianteD();
+            list = objEst.mtdListadoCompletoE();
+
+            gvlistEstudiante.DataSource = list;
+
+            gvlistEstudiante.DataBind();
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+
+            List<clEntidadEstudianteE> listaBuscar = new List<clEntidadEstudianteE>();
+
+            clEntidadEstudianteE objEst = new clEntidadEstudianteE();
+            objEst.Nombres = nombre;
+
+            clEstudianteL objEstudianteL = new clEstudianteL();
+            listaBuscar = objEstudianteL.mtdBuscarEstu(objEst);
+
+            if (listaBuscar.Count != 0)
+            {
+                gvlistEstudiante.DataSource = listaBuscar;
+                gvlistEstudiante.DataBind();
+
+            }
+        }
+    }
+}
